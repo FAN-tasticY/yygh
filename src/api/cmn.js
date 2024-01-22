60df11b075fcae77d56d7320c4899e6ff9247a35
FAN-tasticY <3127574093@qq.com>
2024-01-22 21:30:50 +0800

    day08

--- src/api/cmn.js
+++ src/api/cmn.js
@@ -3,7 +3,7 @@ import service from "@/utils/request";
 export default {
     getInfoByPid(pid){
         return service({
-            url:`http://localhost:8202/admin/cmn/childList/${pid}`
+            url:`/admin/cmn/childList/${pid}`
             ,method:'get'
         })
     }
